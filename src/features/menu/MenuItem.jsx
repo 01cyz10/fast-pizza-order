86683d161023fa0f3ee99104b1a0432f865aa7aa
Yuzhe Chen <chenyuzhe0110@outlook.com>
2023-12-28 12:34:58 +1300

    commit

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,14 +1,33 @@
+import { formatCurrency } from '../../utils/helpers';
+
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  // const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  // return (
+  //   <li>
+  //     <img src={imageUrl} alt={name} />
+  //     <div>
+  //       <p>{name}</p>
+  //       <p>{ingredients.join(', ')}</p>
+  //       <div>
+  //         {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+  //       </div>
+  //     </div>
+  //   </li>
+  // );
 
   return (
     <li>
-      <img src={imageUrl} alt={name} />
+      <img src={pizza.imageUrl} alt={pizza.name} />
       <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
+        <p>{pizza.name}</p>
+        <p>{pizza.ingredients.join(', ')}</p>
         <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+          {!pizza.soldOut ? (
+            <p>{formatCurrency(pizza.unitPrice)}</p>
+          ) : (
+            <p>Sold out</p>
+          )}
         </div>
       </div>
     </li>
