24fba0580fef8c4581dc54d0ea1b4cc24dbbce7f
Yuzhe Chen <chenyuzhe0110@outlook.com>
2024-01-01 18:09:13 +1300

    commit

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -14,9 +14,10 @@ function SearchOrder() {
   return (
     <form onSubmit={handleSubmit}>
       <input
-        placeholder='Search Order #'
+        placeholder="Search Order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="placeholder:text-color-400 focus: w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm ring transition-all duration-300 focus:outline-none focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       ></input>
     </form>
   );
