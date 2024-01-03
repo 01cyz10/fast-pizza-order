172fa9b7411ad243622948adc5528f25ecf0d6c5
Yuzhe Chen <chenyuzhe0110@outlook.com>
2024-01-03 14:58:19 +1300

    commit

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -6,6 +6,7 @@ import { Outlet, useNavigation } from 'react-router-dom';
 function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === 'loading';
+
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
