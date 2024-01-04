8d4d92bca729b5654c2b3283fd1ea922611152d9
Yuzhe Chen <chenyuzhe0110@outlook.com>
2024-01-04 20:59:06 +1300

    commit

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+import { updateName } from './userSlice';
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    // guard clause
+    if (!username) return;
+
+    // update the input value to Redux store when submit
+    dispatch(updateName(username));
+    navigate('/menu');
   }
 
   return (
